2f6c59dfd7237d42890712626425864cad436b29
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-04-27 10:38:50 -0300

    preparando para deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL: process.env.API_URL || "http://localhost:3333/",
 });
 
 /* O axios é uma biblioteca para fazer requisições HTTP que trás funcionalidades
